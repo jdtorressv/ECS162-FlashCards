2fbaece1712f979e59c23c88c6c4ab5427e86225
randalmurphal <ranmurph@gmail.com>
2019-05-18 21:57:38 -0700

    moved files out of file to test with node

--- createDB.js
+++ createDB.js
@@ -1,24 +1,29 @@
 // Globals
-var sqlite3 = require("sqlite3").verbose();  // use sqlite
-var fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();  // use sqlite
+const fs = require("fs"); // file system
 
-var dbFileName = "FlashCards.db"
+const dbFileName = "Flashcards.db";
+
 // makes the object that represents the database in our code
-var db = new sqlite3.Database(dbFileName);
+// object that our Node program communicates with db software
+const db = new sqlite3.Database(dbFileName);  // object, not database.
 
 // Initialize table.
 // If the table already exists, causes an error.
-// Fix the error by removing or renaming PhotoQ.db
-var cmdStr = "CREATE TABLE flashcards (idNum INTEGER UNIQUE NOT NULL PRIMARY KEY)"
+// Fix the error by removing or renaming Flashcards.db
+// When creating table, by default adds 'rowid' with 1, 2, ... -- to quickly access index
+const cmdStr = 'CREATE TABLE Flashcards (userid INT, input TEXT, output TEXT, num_seen INT, num_correct INT)'
 db.run(cmdStr,tableCreationCallback);
 
 // Always use the callback for database operations and print out any
 // error messages you get.
+// This database stuff is hard to debug, give yourself a fighting chance.
 function tableCreationCallback(err) {
-    if (err) {
-	console.log("Table creation error",err);
-    } else {
-	console.log("Database created");
-	db.close();
-    }
+  if (err) {
+      console.log("Table creation error",err);
+  }
+  else {
+    console.log("Database created");
+	  db.close();
+  }
 }
